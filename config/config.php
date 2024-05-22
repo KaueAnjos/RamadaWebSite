2e397b23a496e48c7227dbfac821455cb747b3d3
Kauê Ramada dos Anjos <kaueramada06@gmail.com>
2024-05-22 11:18:55 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // path site
-define('INCLUDE_PATH', 'http://localhost/RamadaWebSite/');
+define('INCLUDE_PATH', 'http://localhost/ramada/');
 
 define('HOST', 'localhost');
 define('USERNAME', 'root');
