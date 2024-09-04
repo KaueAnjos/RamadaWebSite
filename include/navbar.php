21c0ba55e36969c374a3148bbb9a43add25f6d69
KaueAnjos <kaueramada06@gmail.com>
2024-09-04 11:46:20 -0300

    organizing offcanvas with information

--- include/navbar.php
+++ include/navbar.php
@@ -6,7 +6,29 @@
             <li><a href="#" onclick="scrollspy('about');">Sobre mim</a></li>
             <li><a href="#" onclick="scrollspy('portfolio');">Portfólio</a></li>
             <li><a href="#" onclick="scrollspy('contact');">Contato</a></li>
-            <li class="gear"><a href="#"><i class="fas fa-cog"></i></a></li>
+            <li class="gear"><a class="btn" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"><i class="fas fa-cog"></i></a></li>
+
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Ramada WebSite</h5>
+                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <div>
+                        Idioma:
+                    </div>
+                    <div class="dropdown mt-3">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+                            Selecione o idioma
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <li><a class="dropdown-item" href="#">Português Brasil</a></li>
+                            <li><a class="dropdown-item" href="#">Inglês</a></li>
+                            <li><a class="dropdown-item" href="#">Espanhol</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </ul>
     </nav>
 </header>
